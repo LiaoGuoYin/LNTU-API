55f83fa29f6b04fe40a640e0188d8490394d3d2d
LiaoGuoYin <liaoguoyin@live.com>
2020-01-12 23:56:50 +0800

    Add some Django Web for test

---
+++ Spider/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from Spider.Capture.Client import Client
+
+
+def getCET(request):
+    client = Client(1710030215, '****')
+    result_lists = client.getCET()
+    return render(request, 'login.html', {"CETS": result_lists})
+
+
+def getScores(request):
+    # client = Client(1710030105, '****')
+    result_dicts = client.getScores(client)
+    print(result_dicts)
+    return render(request, 'scores.html', {"scores": result_dicts})
+
+
+def login(request):
+    if request.method == "POST":
+        # username = request.POST.get("username")
+        # password = request.POST.get("password")
+        # client = Client(username, password)
+        # client.getScores()
+        return HttpResponse("Processing..")
+    else:
+        return HttpResponse("GET method is coding..")

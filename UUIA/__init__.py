82d416c80cf4881a229d1411a18d58f7a73eefb5
LiaoGuoYin <liaoguoyin@live.com>
2020-02-29 23:27:48 +0800

    remove UUIA

--- UUIA/__init__.py
+++
@@ -1,5 +0,0 @@
-from .exception.action_callback_exception import Callback_exception
-from .exception.config_error_exception import Config_error_exception
-from .exception.lack_necessary_info_exception import Lack_necessary_info_exception
-from .exception.not_implemented_exception import Not_implemented_exception
-from .uuid_index import Uuia

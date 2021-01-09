eb21935a3d68fc1b95b8a80d5f011c532eb9b3db
LiaoGuoYin <liaoguoyin@live.com>
2021-01-09 20:15:37 +0800

    [Code Update] Updated docs and Response-Message for conciseness

--- app/mobile/router.py
+++ app/mobile/router.py
@@ -11,16 +11,20 @@ router = APIRouter()
 async def register_notification(form: schemas.NotificationToken):
     """
         绑定 Notification Token
+    - **token**: 来自 Apple Push Notification Service 的 deviceToken
+    - **username**: 在本系统登录过的教务在线学号
     """
     response = schemas.ResponseT(data=form)
     response.code, response.message = crud.register_notification(form, session=db.session)
     return response
 
 
-@router.post("/notification-remove", response_model=schemas.ResponseT, summary='取消 Notification Token')
+@router.post("/notification-remove", response_model=schemas.ResponseT, summary='取消绑定 Notification Token')
 async def remove_notification(form: schemas.NotificationToken):
     """
         取消绑定 Notification Token
+    - **token**: 来自 Apple Push Notification Service 的 deviceToken
+    - **username**: 在本系统登录过的教务在线学号
     """
     response = schemas.ResponseT(data=form)
     response.code, response.message = crud.remove_notification(form, session=db.session)
